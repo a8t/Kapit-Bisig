8c3d2eeac1f63c0ae5c4f1eb187de7d6e3bb1d49
Andy <andy@gettimesaved.com>
2020-04-20 17:57:06 -0400

    Connect Footer to Sanity

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -1,25 +1,32 @@
 import React from "react"
 import { Footer, Container, Content, Icon } from "bloomer"
+import { useStaticQuery, graphql } from "gatsby"
+import PortableText from "./portableText"
 
 const MainFooter = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site: sanitySiteConfig {
+          _rawFooterText
+        }
+        defaultOgImage: file(relativePath: { eq: "kapitbisig-og.png" }) {
+          childImageSharp {
+            original {
+              src
+            }
+          }
+        }
+      }
+    `
+  )
+
   return (
     <Footer>
       <Container>
-        {/* Built by ZLI */}
         <Content>
-          <p>
-            Kapit-Bisig Laban COVID is a project of{" "}
-            <a
-              href="https://www.facebook.com/BAYANCanada/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              BAYAN Canada
-            </a>
-            .
-          </p>
+          <PortableText blocks={site._rawFooterText} />
         </Content>
-        {/* Built with Gatsby, Bulma, and Bloomer */}
         <Content isSize="small">
           <p>© {new Date().getFullYear()}</p>
         </Content>
