7ef6949d3cb3c7e49ac0196a49529a876c2592ce
thinkbulecount2 <38994650+thinkbulecount2@users.noreply.github.com>
2020-05-02 21:50:22 -0400

    Grouped cities by province in request/volunteer

--- web/src/pages/request.tsx
+++ web/src/pages/request.tsx
@@ -1,58 +1,72 @@
 import React from "react"
-import { Section, Container, Title, Content } from "bloomer"
+import { Section, Container, Content } from "bloomer"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
+import { toKebabCase } from "../utils/toKebabCase"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import CityCard from "../components/CityCard"
+import { Title, Subtitle, Paragraph } from "../components/ds/typography"
 
 const RequestPage = () => {
-  const { cities } = useStaticQuery(
-    graphql`
-      query RequestPageQuery {
-        cities: allSanityCity {
-          nodes {
-            name
-            province {
-              name
-            }
-            requestForm
-            cityLogo {
-              asset {
-                fluid(maxWidth: 700) {
-                  ...GatsbySanityImageFluid
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  )
+	const { cities } = useStaticQuery(
+		graphql`
+		query RequestPageQuery {
+		  cities: allSanityCity {
+		    citiesGroupedByProvince: group(field: province___name) {
+		      provinceName: fieldValue
+		      cities: nodes {
+		        name
+		        requestForm
+		        cityLogo {
+		          asset {
+		            fluid(maxWidth: 700) {
+		              ...GatsbySanityImageFluid
+		            }
+		          }
+		        }
+		      }
+		    }
+		  }
+		}
+		`
+	)
 
-  return (
-    <Layout>
-      <SEO title="Request | Kapit-Bisig Canada" />
-      <Section className="has-background-white">
-        <Container>
-          <Title>Help request</Title>
-          <Content>
-            Select your city to request help. More are coming soon.
-          </Content>
-          {cities.nodes.map(city => {
-            return (
-              <CityCard
-                name={city.name}
-                provinceName={city.province.name}
-                cityLogoAssetFluid={city.cityLogo?.asset.fluid}
-                link={city.requestForm}
-              />
-            )
-          })}
-        </Container>
-      </Section>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<SEO title="Request | Kapit-Bisig Canada" />
+			<section className="container py-8">
+					<Title>Help Request</Title>
+					<Paragraph>Select your city to request help. More are coming soon.</Paragraph>
+
+					<div style={{ display: "flex", flexDirection: "column" }}>
+					{cities.citiesGroupedByProvince.map(({ provinceName, cities }) => {
+					  return (
+								<div
+									style={{
+									  display: "flex",
+									  flexDirection: "column",
+									  paddingBottom: 32,
+									}}
+								>
+								<Subtitle>{provinceName}</Subtitle>
+								{cities.map(city => {
+								  return (
+								    <CityCard
+								      name={city.name}
+								      provinceName={provinceName}
+								      cityLogoAssetFluid={city.cityLogo?.asset.fluid}
+								      link={city.requestForm}
+								    />
+								  )
+								})}
+								</div>
+							)
+						})}
+				</div>
+			</section>
+		</Layout>
+	)
 }
 export default RequestPage
