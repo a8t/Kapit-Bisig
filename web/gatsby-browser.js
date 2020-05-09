c02c972721bf0eafcb14e4d48a609bb58aa9f33a
Andy <andy@gettimesaved.com>
2020-05-09 01:45:52 -0400

    Fix flash of big icons

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -1,1 +1,2 @@
 import "./src/styles/tailwind.css"
+import "@fortawesome/fontawesome-svg-core/styles.css"
--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -5,10 +5,11 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import { library } from "@fortawesome/fontawesome-svg-core"
+import { library, config } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
 import { fas } from "@fortawesome/free-solid-svg-icons"
 
+config.autoAddCss = false
 library.add(fab, fas)
 
 import React from "react"
