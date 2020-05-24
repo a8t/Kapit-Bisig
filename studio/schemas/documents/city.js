532b4358e2fdd49961714057f1a75b40b8a75d9d
thinkbulecount2 <38994650+thinkbulecount2@users.noreply.github.com>
2020-05-24 02:08:57 -0400

    Added missing ending } at Volunteer section

--- studio/schemas/documents/city.js
+++ studio/schemas/documents/city.js
@@ -1,42 +1,42 @@
-import {MdPinDrop} from 'react-icons/lib/md'
+import { MdPinDrop } from 'react-icons/lib/md'
 
 export default {
-  name: 'city', // accidentally made this uppercase...
-  type: 'document',
-  title: 'City',
-  icon: MdPinDrop,
-  fields: [
-    {
-      name: 'name',
-      type: 'string',
-      title: 'Name'
-    },
-    {
-      name: 'province',
-      type: 'reference',
-      to: [{type: 'province'}],
-      title: 'Province'
-    },
-    {
-      name: 'description',
-      type: 'string',
-      title: 'Description'
-    },
-    {
-      name: 'requestForm',
-      type: 'string',
-      title: 'Request Form'
-    },
-    {
-      name: 'volunteerForm',
-      type: 'string',
-      title: 'Volunteer Form'
-    },
-    {
-      name: 'cityLogo',
-      type: 'image',
-      title: 'City Image',
-      description: 'Logo for the city'
-    }
-  ]
+	name: 'city', // accidentally made this uppercase...
+	type: 'document',
+	title: 'City',
+	icon: MdPinDrop,
+	fields: [
+		{
+			name: 'name',
+			type: 'string',
+			title: 'Name'
+		},
+		{
+			name: 'province',
+			type: 'reference',
+			to: [{ type: 'province' }],
+			title: 'Province'
+		},
+		{
+			name: 'description',
+			type: 'string',
+			title: 'Description'
+		},
+		{
+			name: 'requestForm',
+			type: 'string',
+			title: 'Request Form'
+		},
+		{
+			name: 'volunteerForm',
+			type: 'string',
+			title: 'Volunteer Form'
+		},
+		{
+			name: 'cityLogo',
+			type: 'image',
+			title: 'City Image',
+			description: 'Logo for the city'
+		}
+	]
 }
