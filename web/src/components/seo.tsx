7ac1c5643d77f0da7316863b926f857d96f5381f
Andy <andy@gettimesaved.com>
2020-06-01 03:36:21 -0400

    one more time with the OG

--- web/src/components/seo.tsx
+++ web/src/components/seo.tsx
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import og from "../images/kapitbisig-og.png"
+import og from "../images/OG-image.png"
 function SEO({ description, lang, meta, title, ogImage = null }) {
   const { site, defaultOgImage } = useStaticQuery(
     graphql`
