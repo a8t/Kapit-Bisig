82a3cd91db0f3fea76fc6ba065f2d71637a74ccf
Andy <andy@gettimesaved.com>
2020-06-12 01:52:50 -0400

    Redesign youth survey page

--- web/src/pages/youth-survey.tsx
+++ web/src/pages/youth-survey.tsx
@@ -30,18 +30,25 @@ const KBLogoCircle = (
 )
 
 const SurveyHeader = () => (
-  <header className="py-24 sm:max-height-60vh flex items-center text-white relative overflow-y-hidden bg-gray-800">
-    <section className="container">
+  <header className="pt-32 pb-20 sm:max-height-60vh flex items-center text-white relative overflow-y-hidden bg-gray-800">
+    <section className="container flex flex-col">
       <Title color="white" className="lg:text-6xl self-start relative">
-        Kapit-Bisig Youth Survey
+        Kapit-Bisig Filipino Youth Network: COVID-19 Survey
       </Title>
       <Subtitle color="white" className="self-start max-w-2xl">
-        <span className="text-xl lg:text-2xl">
-          Are you a Filipino youth in Canada?
+        <span className="text-xl lg:text-2xl font-normal">
+          Help community organizations best serve your community,
           <br />
-          Fill out the survey below to enter the raffle.
+          enter a $50 gift card raffle.
         </span>
       </Subtitle>
+
+      <Link
+        to="#survey"
+        className=" self-start text-xs sm:text-sm mt-2 bg-white p-2 px-6 shadow-xl border-2 rounded-full z-50 flex justify-center items-center"
+      >
+        Start Survey
+      </Link>
     </section>
   </header>
 )
@@ -52,25 +59,27 @@ const BlockQuote = ({
   authorName,
   authorDescription,
 }) => (
-  <blockquote className="grid bg-white shadow-xl p-8 md:p-12">
-    <div className="relative">
-      <span
-        className="text-6xl -ml-5 -mt-2 text-white mr-2 hidden sm:block absolute top-0 z-1 leading-none"
-        aria-hidden="true"
-        style={{
-          textShadow: `
+  <blockquote className="grid bg-white shadow-xl p-12 max-w-xl mx-auto relative">
+    <span
+      className="text-6xl -ml-5 -mt-2 text-white mr-2 hidden sm:block absolute top-0 z-1 leading-none"
+      aria-hidden="true"
+      style={{
+        textShadow: `
           3px 3px 0 rgba(0,0,0,0.11),
           -1px -1px 0 rgba(0,0,0,0.11),  
          1px -1px 0 rgba(0,0,0,0.11),
          -1px 1px 0 rgba(0,0,0,0.11),
           1px 1px 0 rgba(0,0,0,0.11)`,
-        }}
-      >
-        &ldquo;
-      </span>
-    </div>
+        top: 50,
+        left: 50,
+      }}
+    >
+      &ldquo;
+    </span>
 
-    <p className="mb-4 sm:text-xl italic z-10 relative">{quote}</p>
+    <p className="mb-4 sm:text-xl lg:text-lg 2xl:text-xl italic z-10 relative self-start ">
+      {quote}
+    </p>
 
     <footer className="flex items-center z-10">
       <div className="w-12 h-12 mr-4 rounded-full shadow-md overflow-hidden bg-gray-400">
@@ -100,30 +109,6 @@ const YouthSurveyPage = () => {
 
       <SurveyHeader />
 
-      {/* this part connects the header with the next section */}
-      <div className="bg-gray-800 relative z-20 w-full">
-        <div
-          className="bg-gray-200 absolute w-full"
-          style={{ top: -50, height: 50 }}
-        >
-          <div
-            className="bg-gray-800"
-            style={{
-              borderBottomRightRadius: "20% 50px",
-              height: 50,
-            }}
-          ></div>
-        </div>
-        <div
-          className="bg-gray-200 py-12 relative flex flex-col "
-          style={{
-            borderTopLeftRadius: "20% 50px",
-          }}
-        >
-          {KBLogoCircle}
-        </div>
-      </div>
-
       {!inView && (
         <Link
           to="#survey"
@@ -135,153 +120,89 @@ const YouthSurveyPage = () => {
         </Link>
       )}
 
-      <section className="container grid lg:grid-cols-2 gap-16">
+      <section className="container pt-20 grid lg:grid-cols-2 gap-16">
         <article className="space-y-8 max-w-xl">
           <Subtitle color="dark">
             The situation regarding COVID-19 is worsening and it has a
             significant impact on our Filipino community.
           </Subtitle>
-          <Paragraph color="dark">
-            <span className="font-normal">
-              We, a network of concerned Filipino community youth organizations,
-              want to ensure that we are safe and healthy during this outbreak.
-            </span>
-          </Paragraph>
           <Paragraph>
-            We are launching a survey for Filipino youth to:
-            <ul className="ml-8 my-4 list-disc">
-              <li>
-                Study and understand the impacts of COVID-19 on Filipino youth
-                and their families in Canada
-              </li>
-              <li>
-                Identify specific concerns or needs we can respond to and
-                address
-              </li>
-              <li>
-                Create or revise programming to tailor to the needs of our
-                Filipino-Canadian community
-              </li>
-            </ul>
-          </Paragraph>
-          <div className="h-12" />
-          <Subtitle>About us</Subtitle>
-          <Paragraph>
-            <span className="font-bold">
-              Kapit-Bisig Laban COVID Canada – Youth Network{" "}
-            </span>
-            commits to serving our communities. We hope to gather as much
-            information as possible to inform us on how to provide resources and
-            support each other in this time of crisis.
-          </Paragraph>
-
-          <Paragraph>
-            The network includes:
-            <ul className="ml-8 my-4 list-disc">
-              <li>
-                <Link to="/organizations/anakbayan-canada">
-                  Anakbayan Canada
-                </Link>
-              </li>
-              <li>
-                <Link to="/organizations/anakbayan-canada">FILCASA</Link>
-              </li>
-              <li>
-                <Link to="/organizations/makulay">Makulay atbp.</Link>
-              </li>
-              <li>
-                <Link to="/organizations/ucfsa">
-                  University of Calgary Filipino Students Association
-                </Link>
-              </li>
-              <li>
-                <Link to="/organizations/anak-inc">ANAK, Inc</Link>
-              </li>
-            </ul>
-          </Paragraph>
-
-          <div className="h-12" />
-          <Subtitle>Join us</Subtitle>
-
-          <Paragraph>
-            Other organizations and groups are invited to share the survey
-            amongst their networks of Filipino youth, and join the network to
-            support youth during this time.
-          </Paragraph>
-          <Paragraph>
-            If you would like to get in touch with us, please send an email to{" "}
-            <a href="mailto:youthsurvey@kapitbisig.ca">
-              youthsurvey@kapitbisig.ca
-            </a>
-            .
+            This survey helps us:
+            <ol className="my-4 space-y-4 list-none flex flex-col items-start">
+              {[
+                "Understand the impacts of COVID-19 on Filipino youth and their families in Canada",
+                "Identify specific concerns or needs we can respond to",
+                "Create or revise programming to tailor to the needs of our Filipino-Canadian community",
+              ].map((str, index) => (
+                <li className="flex justify-center items-center">
+                  <span className="mr-4 font-bold text-lg">{index + 1}</span>
+                  {str}
+                </li>
+              ))}
+            </ol>
           </Paragraph>
         </article>
-
-        <section className="max-w-sm ml-auto mr-auto lg:mr-1 flex flex-col space-y-16">
-          <BlockQuote
-            quote="We’d like thank our community partners for launching this survey, and we look forward to learning more on how we can identify and respond to the needs of our community."
-            authorName="Georelle Mendoza"
-            authorDescription="President, FILCASA"
-            avatar={g}
-          />
-          <BlockQuote
-            quote="We recognize the lack of visibility and spaces for queer and trans Filipino youth to be heard and made sure that the survey can be as inclusive to them."
-            authorName="Members"
-            authorDescription="Makulay atbp."
-          />
-          <BlockQuote
-            quote="Ang mga hadlang sa mga mapagkukunan na kinakaharap ng mga migranteng undocumented, at mga kabataan uring manggagawa ay maaari lamang matugunan sa pamamagitan ng pag-iisa"
-            authorName="Nicole Araneta"
-            authorDescription="Chairperson, Anakbayan-Canada"
-            avatar={nicole}
-          />
-        </section>
+        <article className="p-8 bg-white shadow-lg">
+          <Subtitle>Organizers</Subtitle>
+          <ul className="mt-4 space-y-4">
+            <li>
+              <Link to="/organizations/anakbayan-canada">Anakbayan Canada</Link>
+            </li>
+            <li>
+              <Link to="/organizations/anakbayan-canada">FILCASA</Link>
+            </li>
+            <li>
+              <Link to="/organizations/makulay">Makulay atbp.</Link>
+            </li>
+            <li>
+              <Link to="/organizations/ucfsa">
+                University of Calgary Filipino Students Association
+              </Link>
+            </li>
+            <li>
+              <Link to="/organizations/anak-inc">ANAK, Inc</Link>
+            </li>
+          </ul>
+        </article>
       </section>
 
-      <section className="my-24">
-        <div className="bg-gray-300 text-gray-900 mx-auto pt-12 pb-6 px-8 sm:px-16 md:px-24 md:rounded-lg max-w-3xl">
-          <Subtitle>About the survey</Subtitle>
-          <p>
-            The survey will consist of questions focused on the following
-            categories:
-          </p>
-          <ul className="ml-8 my-2 list-disc">
-            <li>demographics</li>
-            <li>economic & employment information</li>
+      <section className="my-24 container">
+        <div className="bg-gray-300 text-gray-900 mx-auto pt-12 pb-6 px-8 sm:px-16 md:px-24 md:rounded-lg grid lg:grid-cols-2 gap-12">
+          <section>
+            <Subtitle>
+              <FontAwesomeIcon icon="poll" className="mr-4" />
+              About
+            </Subtitle>
 
-            <li>education</li>
-            <li>community involvement</li>
-            <li>home life</li>
-            <li>physical and mental health</li>
-          </ul>
-          <Paragraph>
-            The survey should take about 20 minutes to complete.
-          </Paragraph>
+            <Paragraph>
+              After the completion of the survey, participants can opt in to
+              enter a raffle to win a $50 gift card.
+            </Paragraph>
 
-          <Paragraph>
-            You can leave the survey at any time if you no longer wish to
-            participate. If you no longer want your answers included in the
-            survey after you complete it, you can contact us to have them
-            removed.
-          </Paragraph>
+            <Paragraph>
+              The survey should take about 20 minutes to complete.
+            </Paragraph>
 
-          <Paragraph>
-            If you cannot complete the survey online and would like to do it by
-            phone, contact us at our hotline +1 866-275-4046.
-          </Paragraph>
+            <Paragraph>
+              You can leave the survey at any time. If you no longer want your
+              answers included in the survey after completion, contact us.
+            </Paragraph>
+          </section>
 
-          <Paragraph>
-            After the completion of the survey, participants can opt in to enter
-            a raffle to win a $50 gift card.
-          </Paragraph>
-
-          <Paragraph>
-            All answers to this survey will be confidential. Personal
-            information that identifies you will not be shared. Information will
-            be aggregated by Kapit-Bisig Laban COVID Canada to present to the
-            Filipino community to further inform education, programming, and
-            advocacy efforts.
-          </Paragraph>
+          <section>
+            <Subtitle>
+              <FontAwesomeIcon icon="user-shield" className="mr-4" />
+              Privacy and Accessibility
+            </Subtitle>
+            <Paragraph>
+              All answers to this survey will be confidential. Personal
+              information that identifies you will not be shared.
+            </Paragraph>
+            <Paragraph>
+              If you cannot complete the survey online and would like to do it
+              by phone, contact us at our hotline +1 866-275-4046.
+            </Paragraph>
+          </section>
         </div>
       </section>
 
@@ -299,6 +220,53 @@ const YouthSurveyPage = () => {
           height="600"
           style={{ background: "white", border: "1px solid #ccc" }}
         ></iframe>
+      </section>
+
+      <section className="grid lg:grid-cols-3 gap-8 container">
+        <BlockQuote
+          quote="We’d like thank our community partners for launching this survey, and we look forward to learning more on how we can identify and respond to the needs of our community."
+          authorName="Georelle Mendoza"
+          authorDescription="President, FILCASA"
+          avatar={g}
+        />
+        <BlockQuote
+          quote="We recognize the lack of visibility and spaces for queer and trans Filipino youth to be heard and made sure that the survey can be as inclusive to them."
+          authorName="Members"
+          authorDescription="Makulay atbp."
+        />
+        <BlockQuote
+          quote="Ang mga hadlang sa mga mapagkukunan na kinakaharap ng mga migranteng undocumented, at mga kabataan uring manggagawa ay maaari lamang matugunan sa pamamagitan ng pag-iisa"
+          authorName="Nicole Araneta"
+          authorDescription="Chairperson, Anakbayan-Canada"
+          avatar={nicole}
+        />
+      </section>
+      <section className="bg-gray-800 py-12 mt-8">
+        <div className=" container justify-start align-start flex flex-col md:flex-row ">
+          <img
+            src={KBLogo}
+            className="h-24 md:h-48 mr-auto md:mr-8  md:ml-auto"
+          />
+          <div className="max-w-xl  mt-8 md:mt-0 mr-auto">
+            <Subtitle color="white">Join us</Subtitle>
+
+            <Paragraph color="white">
+              Other organizations and groups are invited to share the survey
+              amongst their networks of Filipino youth, and join the network to
+              support youth during this time.
+            </Paragraph>
+            <Paragraph color="white">
+              If you would like to get in touch with us, please send an email to{" "}
+              <a
+                href="mailto:youthsurvey@kapitbisig.ca"
+                className=" text-teal-300 font-bold"
+              >
+                youthsurvey@kapitbisig.ca
+              </a>
+              .
+            </Paragraph>
+          </div>
+        </div>
       </section>
     </Layout>
   )
