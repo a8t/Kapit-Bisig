9f94c02347a0684ce4487d3256f1f7551e84babf
Andy <andy@gettimesaved.com>
2020-06-22 21:34:50 -0400

    Add PDF schema types

---
+++ studio/schemas/objects/pdf.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'PDF',
+  name: 'pdf',
+  accept: '.pdf',
+  type: 'file',
+  fields: [
+    {
+      name: 'description',
+      type: 'string',
+      title: 'Description'
+    }
+  ]
+}
